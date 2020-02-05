53550a09697dce7cda82a0f3abedb661da794343
John King <johnking@seattleacademy.org>
2020-02-05 11:12:42 -0800

    add web page server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-consloe.log("hello world");
+const express = require('express')
+const app = express()
+const port = 1506
+let counter = 0
+
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => res.send('Hello JOhn!'))
+
+app.get('/counter', function (req, res) {
+	counter = counter + 1;
+  res.send('counter' + counter)
+})
+
+
+app.get('/date', function (req, res) {
+	let d = new Date();
+  res.send('Date' + d)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
